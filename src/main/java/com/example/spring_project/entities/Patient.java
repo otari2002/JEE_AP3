9afac3a9524335e2c8a9ed730bae876d3ff42d9b
otari2002 <omar.ibn.tahri@gmail.com>
2024-03-18 09:38:13 +0000

    more changes

--- src/main/java/com/example/spring_project/entities/Patient.java
+++ src/main/java/com/example/spring_project/entities/Patient.java
@@ -2,6 +2,8 @@ package com.example.spring_project.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
 @Entity
@@ -14,6 +16,7 @@ public class Patient {
     private Long id;
     private String nom;
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
     private int score;
